4cdda0c89dcb6be7286f6fbd83e0cdf665a494c7
Capella87 <contact@capelladev.com>
2022-08-15 01:53:28 +0900

    Implement put in AnnouncementDetailView

--- announcements/views.py
+++ announcements/views.py
@@ -31,9 +31,7 @@ class ListAnnouncements(APIView):
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
 class AnnouncementDetailView(APIView):
-
     queryset = Announcement.objects.all()
-    authentication_classes = [authentication.SessionAuthentication, authentication.TokenAuthentication, ]
     permission_classes = [permissions.IsAdminUser, ]
 
     def get(self, request, pk):
@@ -41,6 +39,23 @@ class AnnouncementDetailView(APIView):
         serializer = AnnouncementSerializer(query)
         return Response(serializer.data)
 
+    def put(self, request, pk):
+        announcement = get_object_or_404(Announcement, pk=pk)
+        data = request.data
+
+        obje = {
+            'title': data['title'],
+            'description': data['description'],
+            'is_visible': data['is_visible'],
+            'is_important': data['is_important'],
+            'writer': announcement.writer
+        }
+        serializer = AnnouncementSerializer(announcement, data=obje)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     def delete(self, request, pk):
         target = get_object_or_404(Announcement, pk=pk)
         target.delete()
@@ -49,7 +64,6 @@ class AnnouncementDetailView(APIView):
 class AnnouncementCheckView(APIView):
     # 00-15 announcement_id인 announcement 수정 (important, visible)
     # 관리자만 접근 가능
-    authentication_classes = [authentication.SessionAuthentication, authentication.TokenAuthentication, ]
     permission_classes = [permissions.IsAdminUser, ]
 
     def put(self, request, pk):
@@ -58,8 +72,9 @@ class AnnouncementCheckView(APIView):
         obj = {
             "title": query.title,
             "description": query.description,
-            "important" : data["is_important"],
-            "visible" : data["is_visible"]
+            "is_important": data["is_important"],
+            "is_visible": data["is_visible"],
+            "writer": query.writer,
         }
         serializer = AnnouncementSerializer(query, data=obj)
         if serializer.is_valid():
